d9335d04f5ada34940522fbb350312cd22068126
Wilder Cossio <w.cossio.r@gmail.com>
2023-01-19 19:56:18 -0400

    estilizando un poco

--- src/navigator/StackNavigator.tsx
+++ src/navigator/StackNavigator.tsx
@@ -3,16 +3,26 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { Pagina2Screen } from '../screens/Pagina2Screen';
 import { Pagina1Screen } from '../screens/Pagina1Screen';
 import { Pagina3Screen } from '../screens/Pagina3Screen';
-import { NavigationContainer } from '@react-navigation/native';
 
 const Stack = createStackNavigator();
 
 export const StackNavigator = () => {
     return (
-        <Stack.Navigator>
-            <Stack.Screen name="Pagina1Screen" component={Pagina1Screen} />
-            <Stack.Screen name="Pagina2Screen" component={Pagina2Screen} />
-            <Stack.Screen name="Pagina3Screen" component={Pagina3Screen} />
+        <Stack.Navigator
+        screenOptions={{
+            headerStyle:{
+                elevation:0,
+                shadowColor: 'transparent'
+            },
+            cardStyle:{
+                backgroundColor: 'white'
+            } 
+        }}
+
+        >
+            <Stack.Screen name="Pagina1Screen" options={{ title: 'Pagina 1', headerTitleAlign: 'center'}} component={Pagina1Screen} />
+            <Stack.Screen name="Pagina2Screen" options={{ title: 'Pagina 2', headerTitleAlign: 'center'}} component={Pagina2Screen} />
+            <Stack.Screen name="Pagina3Screen" options={{ title: 'Pagina 3', headerTitleAlign: 'center'}} component={Pagina3Screen} />
         </Stack.Navigator>
     );
 }
--- src/screens/Pagina2Screen.tsx
+++ src/screens/Pagina2Screen.tsx
@@ -1,5 +1,5 @@
 import { useNavigation } from '@react-navigation/native';
-import React from 'react'
+import React, { useEffect } from 'react'
 import { View, Text, Button } from 'react-native';
 import { styles } from '../theme/appTheme';
 
@@ -7,9 +7,17 @@ export const Pagina2Screen = () => {
 
     const navigator = useNavigation<any>();
 
+    useEffect(() => {
+        navigator.setOptions({
+            title: "Hola miau",
+            headerBackTitle: "Atras"
+        });
+    }, [])
+    
+
     return (
         <View style={styles.globalMargin}>
-            <Text> Pagina2Screen </Text>
+            <Text style={styles.title}> Pagina2Screen </Text>
             <Button
                 title='Ir pagina 3'
                 onPress={() => navigator.navigate('Pagina3Screen')}
