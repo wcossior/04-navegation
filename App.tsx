d5ed156c2b73ecc7da8c8a3a2f668bd2b2be9627
Wilder Cossio <w.cossio.r@gmail.com>
2023-03-08 20:17:37 -0400

    Drawer navigator

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react'
-import { StackNavigator } from './src/navigator/StackNavigator';
+// import { StackNavigator } from './src/navigator/StackNavigator';
+import { BasicDrawerNavigatorMiau } from './src/navigator/BasicDrawerNavigator';
 
 const App = () => {
     return (
         <NavigationContainer>
-            <StackNavigator/>
+            {/* <StackNavigator/> */}
+            <BasicDrawerNavigatorMiau />
         </NavigationContainer>
     )
 }
---
+++ src/navigator/BasicDrawerNavigator.tsx
@@ -0,0 +1,14 @@
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { StackNavigator } from './StackNavigator';
+import { SettingsScreen } from '../screens/SettingsScreen';
+
+const Drawer = createDrawerNavigator();
+
+export const BasicDrawerNavigatorMiau = () => {
+    return (
+        <Drawer.Navigator>
+            <Drawer.Screen name="StackNavigator" component={StackNavigator} />
+            <Drawer.Screen name="SettingsScreen" component={SettingsScreen} />
+        </Drawer.Navigator>
+    );
+}
